081ccd44b7554e7d87900eb6625a0268601aa1db
Sam <murray_sam@hotmail.com>
2018-10-22 10:58:20 -0700

    Add files via upload

--- hs.js
+++ hs.js
@@ -2,30 +2,52 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);  //websockets
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+var sensor = require('node-dht-sensor');
+var HEAT_OUTPUT = new Gpio(19, 'out'); //use GPIO pin 4, and specify that it is output
+
+//var VENT_OUTPUT = new Gpio(4, 'out');// Future Ventilation unit activation output
+//var ZONE1 = new Gpio(16, 'out');// Future Zone 1 temperature inputs for AM2302
+//var ZONE2 = new Gpio(4, 'out');// Future Zone 2 temperature inputs for AM2302
+//var ZONE3 = new Gpio(4, 'out');// Future Zone 3 temperature inputs for AM2302
+//var ZONE4 = new Gpio(4, 'out');// Future Zone 4 temperature inputs for AM2302
+//var ZONE5 = new Gpio(4, 'out');// Future Zone 5 temperature inputs for AM2302
+
 
 let heater_temp = 70;  //default value to set the temperature till the sensors are polled.
 let heat_setpoint = 72; //default setpoint for the temperature of the house.  So programs starts with the heat off.
-let temp_check_interval = 1000 * 5;   // milliseconds mulitplied by the number of seconds between temperature checks 
+let temp_check_interval = 1000 * 10;   // milliseconds mulitplied by the number of seconds between temperature checks 
+let zone1_temp=0;
+
+sensor.read(22, 12, function(err, temperature, humidity) {
+    console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
+    });
 
 
 //// check the temperature to turn on the heater every interval
  setInterval(function(){
- 	var heater_temp=heater_temp;  //pulling in global variable
- 	var heat_setpoint = heat_setpoint; //pulling in global variable
+ 	var temp = heater_temp;  //pulling in global variable
+ 	var setpoint = heat_setpoint; //pulling in global variable
  	var boiler_active = "";
 
- 	if (heater_temp < _heat_setpoint){  //evalute the temperature, if it is less than the setpoint, turn on the heat.
- 		LED.writeSync(1); //set pin state to 1 (turn heater on)
+ 	sensor.read(22, 12, function(err, temperature, humidity) {
+ 		heater_temp = temperature *1.8+32;
+ 		console.log(zone1_temp);
+        console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
+	});
+
+
+
+ 	if (temp < setpoint){  //evalute the temperature, if it is less than the setpoint, turn on the heat.
+ 		HEAT_OUTPUT.writeSync(1); //set pin state to 1 (turn heater on)
  		} else {
- 		LED.writeSync(0); // set pin state to 0 (turns heater off)
+ 		HEAT_OUTPUT.writeSync(0); // set pin state to 0 (turns heater off)
  		}
- 	if (LED.readSync()) // looks at the state of the pin to determine if the heater is being sent an on/off signal
+ 	if (HEAT_OUTPUT.readSync()) // looks at the state of the pin to determine if the heater is being sent an on/off signal
  		{boiler_active="ON";  //  The actual state of the output is being looked at to report its status 
  		} else {                 
  		boiler_active="OFF"};
 
- 	console.log ("Setpoint is: " +heat_setpoint + "   Current Temperature is: " + heater_temp +"   Boiler is: "+boiler_active);
+ 	console.log ("Setpoint is: " +setpoint + "   Current Temperature is: " + temp +"   Boiler is: "+boiler_active);
  	},temp_check_interval);
 
 
@@ -48,10 +70,10 @@ io.on('connection', function(socket){
   	io.emit('chat message',msg);
   	if (msg == 'true'){
   		console.log("this is true")
-  		LED.writeSync(1); //set pin state to 0 (turn LED off)
+  		HEAT_OUTPUT.writeSync(1); //set pin state to 0 (turn LED off)
   	} else{
   		console.log('this false')
-  		LED.writeSync(0);   //set pin state to 1 (turn LED on)
+  		HEAT_OUTPUT.writeSync(0);   //set pin state to 1 (turn LED on)
   	}
   });
 //////////////
@@ -61,8 +83,8 @@ io.on('connection', function(socket){
  ////// recieves switch on or off from client, 
 	// socket.on('switch', function(data){
 	// 	switchvalue = data;
-	// 	if (switchvalue != LED.readSync()) { //only change the LED if the status is changed
-	// 		LED.writeSync(switchvalue);
+	// 	if (switchvalue != HEAT_OUTPUT.readSync()) { //only change the LED if the status is changed
+	// 		HEAT_OUTPUT.writeSync(switchvalue);
 	// 		console.log('Gpio changed');
 	// 	}
 
